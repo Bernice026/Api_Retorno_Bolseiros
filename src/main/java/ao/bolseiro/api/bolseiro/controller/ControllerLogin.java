a03a9e2bce1e65ebadc99bcf5612cbe075e827f7
Bernice Sachilombo <enya.berenice75@gmail.com>
2024-05-11 17:29:22 +0100

    novo commit

---
+++ src/main/java/ao/bolseiro/api/bolseiro/controller/ControllerLogin.java
@@ -0,0 +1,18 @@
+package ao.bolseiro.api.bolseiro.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class ControllerLogin {
+	
+	
+	@PostMapping("/")
+	public void login() {
+		
+		
+	}
+
+}
